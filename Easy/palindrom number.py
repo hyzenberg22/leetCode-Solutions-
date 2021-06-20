fe6ae1a5b954c7b43dd94a811c944eaedab0492d
Shubhajit Sarkar <43705275+shubhajit22@users.noreply.github.com>
2021-06-20 14:31:52 +0530

    palindrom number

---
+++ Easy/palindrom number.py
@@ -0,0 +1,36 @@
+# This is the program the compiler is showing correct but the not running in the leet code 
+
+# ------------sol:-1-------------
+# ------------This is the string reversing approach 
+# def palindrom(inp):
+#     temp = str(inp)[::-1]
+#     if str(inp)[0]=="-":
+#         return "false"
+#     else:
+#         if (inp >= -2147483648 and inp<= 2147483647):
+#             if str(inp) == temp:
+#                 return "true"
+#             else:
+#                 return "false"
+#         else:
+#             return 0
+
+
+# ----------sol:-2----------
+#-----------This is the mathematical approach of reversing the number 
+
+def palindrom(inp):
+    if inp < 0:
+        return False
+    else:
+        c = inp
+        b = 0
+        while c:
+            b = b*10 + c%10
+            c//=10
+        return b == inp
+    
+        
+
+if __name__ == '__main__':
+    print(palindrom(121))
